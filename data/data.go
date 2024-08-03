8ac85fe6190d21c6f7982fdce2610cf83200a533
韩杰 <hanjie@blocgo.com>
2024-08-03 15:11:42 +0800

    refactor: Remove commented out database connection string

--- data/data.go
+++ data/data.go
@@ -11,7 +11,6 @@ var db *sql.DB
 func init() {
 
 	var err error
-	// dsn: "host=82.157.51.205 user=gaussdb password=zcpt123_#Shuyo dbname=xt_base port=5432 sslmode=disable TimeZone=Asia/Shanghai"
 	db, err = sql.Open("postgres", "host=127.0.0.1 user=postgres	password=mysecretpassword dbname=postgres port=5432 sslmode=disable TimeZone=Asia/Shanghai")
 
 	if err != nil {
