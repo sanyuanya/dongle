4f74743ece2bf8cdf0ce22a7702bd6b5dc09fda1
韩杰 <3068271240@qq.com>
2024-11-05 14:59:15 +0800

    fix: 设置上传文件的大小配额

--- main.go
+++ main.go
@@ -29,7 +29,12 @@ import (
 
 func main() {
 	// Initialize a new Fiber app
-	app := fiber.New()
+	app := fiber.New(fiber.Config{
+		BodyLimit:     100 * 1024 * 1024,
+		AppName:       "dongle",
+		CaseSensitive: true,
+	})
+	
 	app.Use(requestid.New())
 
 	app.Use(logger.New(logger.Config{
